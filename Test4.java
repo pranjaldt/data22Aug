921e33acfe0ebe71304fe3668783b56500491734
pranjaldt <pranjald20@gmail.com>
2018-08-22 16:17:37 +0530

    message

---
+++ Test4.java
@@ -0,0 +1,29 @@
+package pack4;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+public class Test4 {
+    public String baseUrl = "http://demo.guru99.com/test/newtours/";
+    String driverPath = "C:\\geckodriver.exe";
+    public WebDriver driver ; 
+     @AfterTest                            //Jumbled
+      public void terminateBrowser(){
+          driver.close();
+      }
+     @BeforeTest                            //Jumbled
+      public void launchBrowser() {
+          System.out.println("launching firefox browser"); 
+          System.setProperty("webdriver.firefox.marionette", driverPath);
+          driver = new FirefoxDriver();
+          driver.get (baseUrl);
+      }
+      @Test                                //Jumbled
+      public void verifyHomepageTitle() {
+          String expectedTitle = "Welcome: Mercury Tours";
+          String actualTitle = driver.getTitle();
+          Assert.assertEquals(actualTitle, expectedTitle);
+     }
+      
+}
